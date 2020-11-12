383b3ac79f05653982a07f576474d0d736145891
andreivorotelenko <andrei.vorotelenko@yandex.com>
2020-11-12 14:15:48 +0500

    Первая простая программа на java

---
+++ home_works/book/izi_prog.java
@@ -0,0 +1,10 @@
+/* 
+	это простая программа на java
+	присвоить исходному файлу имя "izi_prog.java"
+*/
+class Example {
+	//эта программа начинается с вызова метода main()
+	public static void main (String args[]){
+		System.out.println("Простая программа на Java");
+	}
+}
