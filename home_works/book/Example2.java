daa37c7434a07386e37e41971b85d00e8a3283ca
andreivorotelenko <andrei.vorotelenko@yandex.com>
2020-11-16 08:41:21 +0500

    Пример второй простой программы

---
+++ home_works/book/Example2.java
@@ -0,0 +1,18 @@
+/* 
+	Это еще один пример короткой программы.
+	Присвоить исходному файлу имя "Example.java".
+*/
+class Example2 {
+	public static void main (String atgs[]){
+		int num; //в этой строке кода объявляется
+			 //переменная с именем num
+		num = 100;//в этой строке кода переменной num
+			  //присваивается значение 100
+
+		System.out.println("Это переменная num : " + num);
+		num = num * 2;
+	
+		System.out.print("Значение переменной num * 2 равно ");
+		System.out.println(num);
+	}
+}
