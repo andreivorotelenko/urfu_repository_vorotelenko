18b793d0a98e14562202dfdfedb95ae2ad8b2a16
andreivorotelenko <andrei.vorotelenko@yandex.com>
2020-11-16 09:59:03 +0500

    Простая программа с циклами

---
+++ home_works/book/ForTest.java
@@ -0,0 +1,12 @@
+/* 
+	Продемнстрировать применение цикла for
+	Присвоить исходному файлу имя "ForTest.java"
+*/
+class ForTest {
+	public static void main (String atgs[]) {
+		int x;
+
+		for (x = 0; x<10; x = x+1)
+			System.out.println("Значение x: " + x);
+	}	
+}
