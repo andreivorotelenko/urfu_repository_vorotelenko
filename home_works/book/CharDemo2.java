16063fb5cfca20c24f6f321d7004c202f352ef39
andreivorotelenko <andrei.vorotelenko@yandex.com>
2020-11-17 08:55:07 +0500

    вторая необязательная программа на char

---
+++ home_works/book/CharDemo2.java
@@ -0,0 +1,13 @@
+//символьные переменные ведут себя
+//как целочисленные значения
+class CharDemo2 {
+	public static void main (String args[]) {
+		char ch1;
+
+		ch1 = 'X';
+		System.out.println("ch1 содержит " + ch1);
+
+		ch1++; // увеличить на единицу значение переменной ch1
+		System.out.println("ch1 теперь содержжит " + ch1);
+	}
+}
