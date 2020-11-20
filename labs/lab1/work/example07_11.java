83ad7a352f58b84cb2c1266757e5427bda251919
and <andrei.vorotelenko@yandex.kz>
2020-11-20 14:25:34 +0600

    доделал дз

--- labs/lab1/work/example07_11.java
+++ labs/lab1/work/example07_11.java
@@ -1,6 +1,10 @@
-class example6 {
-	static double a = 10.0, b = 4.0, c;
+class example07_11 {
+	static double a = 10.0, b = 5.0, c;
 	
+	public static double new_(){
+		return c = Math.exp(b*Math.log(a));
+	}
+
 	public static double hyp() {
 		return c = Math.sqrt(a * a + b * b);
 	}
@@ -9,5 +13,6 @@ class example6 {
 		System.out.println("катет а=" + a);
 		System.out.println("катет b=" + b);
 		System.out.println("катет c=" + hyp());
+		System.out.println("a^b=" + new_());
 	}
 }
---
+++ labs/lab1/work/example07_13.java
@@ -0,0 +1,13 @@
+class example07_13 {
+	static double a = 10.0, b = 3.0, c;
+	
+	public static double new_(){
+		return c = Math.pow(a,b);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("катет а=" + a);
+		System.out.println("катет b=" + b);
+		System.out.println("катет a^b=" + new_());
+	}
+}
